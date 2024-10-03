f73a985239ac7293ffc085cb87a0d048cd3dcdda
runayasuga <126374258+runayasuga@users.noreply.github.com>
2024-10-03 15:08:10 +0900

    2-6

---
+++ Study/2-6/Task2_6.java
@@ -0,0 +1,42 @@
+public class Task2_6 {
+
+
+    public static void main(String[] args) {
+
+
+        
+        int hogeInt = 50;
+
+
+        
+        String hogeString = "テスト";
+
+
+        
+        // [double型の hogeDouble という名前の変数を宣言し、値「0.8」で初期化]
+        double hogeDouble = 0.8;
+
+
+        
+        hogeInt = 100;
+
+
+        
+        hogeString = "良くできました！";
+
+
+       
+         System.out.println(hogeInt);
+
+
+       
+        // [hogeStringという変数に格納されている文字列をコンソールに出力するもの]
+        System.out.println(hogeString);
+
+
+        
+        System.out.println(hogeDouble);
+
+
+    }
+}
