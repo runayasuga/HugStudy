9aa7fa62ff495e3b7d6099880bcc9bf3d4977849
runayasuga <126374258+runayasuga@users.noreply.github.com>
2024-10-03 18:37:24 +0900

    2-8

---
+++ Study/2-8/Task2_8.java
@@ -0,0 +1,59 @@
+public class Task2_8 {
+
+
+    public static void main(String[] args) {
+
+
+
+
+    // ① 5 と 16 を加算した値を代入する、変数 plus を作成して下さい。
+    int plus = 5 + 16;
+
+
+
+
+    // ② 27 から 7 を減算した値を代入する、変数 minus を作成して下さい。
+    int minus = 27 - 7;
+
+
+
+
+    // ③下記の処理について、何をしているのかコメントを記入して下さい。
+    // [10と2を乗算してその結果をmultiplyに代入している。]
+    int multiply = 10 * 2;
+
+
+
+
+    // ④ 20 を 6 で割った余りを代入する、変数 remi を作成して下さい。
+    int remi = 20 % 6;
+
+
+
+
+    // ⑤下記の処理について、何をしているのかコメントを記入して下さい。
+    // [文字列 中央 を変数 town に代入している。文字列 線 を変数 line に代入している。]
+    String town = "中央";
+    String line = "線";
+
+
+
+
+    // ⑥ ⑤で作成した変数を連結させた値を代入する変数 train を作成して下さい。
+    String train = town + line;
+
+
+
+
+    // ⑦ 変数plus, minus, multiply, remi, train をそれぞれ出力して下さい。
+    System.out.println("plus" + plus);
+    System.err.println("minus" + minus);
+    System.err.println("multiply" + multiply);
+    System.err.println("remi" + remi);
+    System.err.println("train" + train);
+
+
+
+
+    }
+}
