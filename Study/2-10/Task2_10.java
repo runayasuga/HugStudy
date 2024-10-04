e4aefedb33a12aae07c8a9264dc9a5297e6e9738
runayasuga <126374258+runayasuga@users.noreply.github.com>
2024-10-04 15:21:25 +0900

    2-10

---
+++ Study/2-10/Task2_10.java
@@ -0,0 +1,44 @@
+public class Task2_10 {
+
+    public static void main(String[] args) {
+
+        // ① 「みかん」、「りんご」、「ぶどう」、「メロン」の値を設定した配列 fruits を作成してください。
+        String[] fruits = {"みかん","りんご","ぶどう","メロン"};
+
+
+        // ② for文を使って①で作成した配列を出力しなさい。
+        for (String fruit : fruits){
+            System.out.println(fruit);
+        }
+
+        // ③ 以下のwhile文の処理について、何をしているのかコメントを記入してください。
+        /*
+        * [1から50までの数字を出力]
+        */
+        int i = 1;
+        while(i <= 50) {
+            System.out.print(i);
+            i++;
+        }
+        System.out.println();
+
+
+        /* ④ 行の最初に「段数」と「||」を追加したものを表示させるプログラムを作成しなさい。
+        * 5 || 5 | 10 | 15 | 20 | 25 | 30 | 35 | 40 | 45 |
+        * 6 || 6 | 12 | 18 | 24 | 30 | 36 | 42 | 48 | 54 |
+        * 7 || 7 | 14 | 21 | 28 | 35 | 42 | 49 | 56 | 63 |
+        * 8 || 8 | 16 | 24 | 32 | 40 | 48 | 56 | 64 | 72 |
+        * 9 || 9 | 18 | 27 | 36 | 45 | 54 | 63 | 72 | 81 |
+        */
+       for (int j = 5; j <= 9; j++) {
+        System.out.println(j + " || ");
+        for (int k = 1; k <= 9; k++){
+            System.out.println(j * k);
+            if (k < 9) {
+                System.out.println(" | ");
+            }
+       }
+       System.out.println(" |");
+       }
+    }
+}
