10d872a0b54944be28ab55085a705a42f3de6d09
runayasuga <126374258+runayasuga@users.noreply.github.com>
2024-10-09 17:36:12 +0900

    2-19

--- Study/2-9/Task2_9.java
+++ Study/2-9/Task2_9.java
@@ -1,15 +1,14 @@
 public class Task2_9 {
 
   // 定数（アカウント情報）
-  private static final String USER_NAME = "alice";
-  private static final String USER_PASSWORD = "alice123";
+   private static final String USER_NAME = "alice"; 
+   private static final String USER_PASSWORD = "alice123"; 
 
   // 定数（メッセージ）
-  private static final String CONST_MSG_SUCCESS = "ログイン成功です。";
-  private static final String CONST_MSG_ERROR_NAME = "名前に誤りがあります。";
-  private static final String CONST_MSG_ERROR_PASS = "パスワードに誤りがあります。";
-  private static final String CONST_MSG_ERROR_INPUT = "入力情報に誤りがあります。";
-
+    private static final String CONST_MSG_SUCCESS = "ログイン成功です。";
+    private static final String CONST_MSG_ERROR_NAME = "名前に誤りがあります。";
+    private static final String CONST_MSG_ERROR_PASS = "パスワードに誤りがあります。";
+    private static final String CONST_MSG_ERROR_INPUT = "入力情報に誤りがあります。";
   
 
   public static void main(String[] args) {
@@ -22,25 +21,25 @@ public class Task2_9 {
     // ① 「name」の値が「USER_NAME」と等しく、「 pass 」の値が「USER_PASSWORD」と等しい場合。
     // 定数を使用して「 ログイン成功です。 」と出力して下さい。
     if (name.equals(USER_NAME) && pass.equals(USER_PASSWORD)) {
-        System.err.println("CONST_MSG_SUCCESS");
+        System.out.println(CONST_MSG_SUCCESS);
     }
 
     // ② 「USER_NAME」の値のみ等しい場合。
     // 定数を使用して「 パスワードに誤りがあります。 」 と出力して下さい。
     else if (name.equals(USER_NAME)) {
-        System.err.println("CONST_MSG_ERROR_PASS");
+        System.out.println(CONST_MSG_ERROR_PASS);
     }
 
     // ③ 「USER_PASSWORD」の値のみ等しい場合。
     //定数を使用して「 名前に誤りがあります。 」と出力して下さい。
     else if (pass.equals(USER_PASSWORD)) {
-        System.err.println("CONST_MSG_ERROR_NAME");
+        System.out.println(CONST_MSG_ERROR_NAME);
     }
 
     // ④ 「USER_NAME」も「USER_PASSWORD」の値も間違っていた場合。
     //定数を使用して「 入力情報に誤りがあります。 」と出力して下さい。
     else {
-        System.err.println("CONST_MSG_ERROR_INPUT");
+        System.out.println(CONST_MSG_ERROR_INPUT);
     }
 
   }
